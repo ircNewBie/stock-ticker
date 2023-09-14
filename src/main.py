43f67a5b867c8dcbdb5af44df817e422bbcc5ea6
bbungalan@gmail.com <bbungalan@gmail.com>
2023-09-15 00:26:03 +0800

    saving to csv and caldulating smas working

--- src/main.py
+++ src/main.py
@@ -1,23 +1,70 @@
 from utils.data import Data
-from dotmap import DotMap
+from datetime import date, timedelta
+import argparse
+
+
+data = Data("tiingo")
+rawData = dict() 
+processedData = dict()
 
 tickers = [
     "TSLA",  # TESLA
     "NVDA",  # NVIDIA CORP
     "AVGO",  # Broadcom
-    "AMD",  # ADVANCED MICRO DEVICES INC
-    "BABA"  # ALIBABA GROUP HOLDING LTD
+    "AMD",   # ADVANCED MICRO DEVICES INC
+    "BABA"   # ALIBABA GROUP HOLDING LTD
 ]
 
-if __name__ == "__main__":
-    url = "https://api.tiingo.com/tiingo/daily/TSLA/prices"
-    data = Data("tiingo")
+
+csvFolderPath = "../data/raw"
+processedCsvFolderPath = "../data/processed"
+
+def fetchData():
+    nDays = 60
+    startDate = date.today() - timedelta(days=nDays)
+    endDate = date.today()
 
     for ticker in tickers:
-        tickerUrl = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices"
-        # print(tickerUrl)
-        rawData = data.fetch(tickerUrl)
-        tickerData = DotMap(rawData)
-        print(ticker, ":", tickerData.date, " ", tickerData.close)
+        print("fetching " + ticker)
+        tickerUrl = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices?startDate={startDate}&endDate={endDate}"
+        tickerRawData = data.convert2DataFrame(data.fetch(tickerUrl))
+        rawData[ticker] = tickerRawData
 
-    # print(rawData.close)
+        # Generate derived dframe (SMAs) 
+        processedData[ticker] = data.calcSMA( rawData[ticker], "close", 5 )
+        processedData[ticker] = data.calcSMA( rawData[ticker], "close", 20 )
+
+        print(processedData[ticker])
+        # Saving raw data frame to csv
+        print("Saving raw data ...")
+        if data.saveDf2Csv(rawData[ticker], csvFolderPath, ticker + ".csv"):
+            print("Success")
+        else:
+            print("Failed")
+
+         # Saving derived / processed  dframe  to csv
+        print("Saving derived data ...")
+        if data.saveDf2Csv(rawData[ticker], processedCsvFolderPath, ticker + ".csv"):
+            print("Success")
+        else:
+            print("Failed")
+
+def main():
+    parser = argparse.ArgumentParser(description="Command-Line options")
+    parser.add_argument("--fetch-data", action="store_true", help="Pull market data from api.")
+    parser.add_argument("--csv-path", action="store_true", help="Show full file path location of CSV files.")
+
+    args = parser.parse_args()
+
+    if args.fetch_data:
+        fetchData()
+    
+    if args.csv_path:
+        print ("Raw CSV files are saved in " +csvFolderPath)
+        print ("Processed CSV files are saved in " +processedCsvFolderPath)
+
+
+    
+
+if __name__ == "__main__":
+    main()
--- src/utils/data.py
+++ src/utils/data.py
@@ -1,5 +1,8 @@
+import os
 import requests
 from utils.config import Config
+import pandas as pd
+
 
 config_file_path = "../config/config.json"
 
@@ -9,16 +12,73 @@ class Data:
         self.config = Config(config_file_path)
         self.dataProvider = dataProvider
 
+
+
     def fetch(self, apiUri):
         apiKey = self.config.getApiKey(self.dataProvider)
-
         headers = {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization' : f'Token {apiKey}'
+
         }
 
-        requestResponse = requests.get(
-            apiUri + "?token=" + apiKey,
-            headers=headers
-        )
+        requestResponse = requests.get(apiUri, headers=headers)
+        return requestResponse.json()
 
-        return requestResponse.json()[0]
+
+    def calcSMA (self, df, price, days):
+        '''
+        Adds moving averages to the existing dataframe \n
+        
+        Arguments:
+             df:        the source data frame
+             price:     price column where averages are based on
+                        eg: close, high, low, etc...
+             days:      the number of days to calculate 
+                        eg: 20-day SMA, days = 20
+        '''
+        df[f'{days}-Day-SMA'] = df[price].rolling(window=days).mean()
+
+        return df
+
+
+
+    def convert2DataFrame(self, data):
+        '''
+        Converts rawaDta into a dataFrame from a array of price object \n
+        Example \n
+        data = [
+            {'date': '2023-09-11T00:00:00.000Z', 'close': 273.58, 'high': 274.85, 'low': 260.61, 'open': 264.27, 'volume': 174667852, 'adjClose': 273.58, 'adjHigh': 274.85, 'adjLow': 260.61, 'adjOpen': 264.27, 'adjVolume': 174667852, 'divCash': 0.0, 'splitFactor': 1.0},
+            {'date': '2023-09-12T00:00:00.000Z', 'close': 267.48, 'high': 278.39, 'low': 266.6, 'open': 270.76, 'volume': 135999866, 'adjClose': 267.48, 'adjHigh': 278.39, 'adjLow': 266.6, 'adjOpen': 270.76, 'adjVolume': 135999866, 'divCash': 0.0, 'splitFactor': 1.0},
+            {'date': '2023-09-13T00:00:00.000Z', 'close': 271.3, 'high': 274.98, 'low': 268.1, 'open': 270.07, 'volume': 111164741, 'adjClose': 271.3, 'adjHigh': 274.98, 'adjLow': 268.1, 'adjOpen': 270.07, 'adjVolume': 111164741, 'divCash': 0.0, 'splitFactor': 1.0}
+        ]
+        '''
+        df = pd.DataFrame(data)
+        df['date'] = pd.to_datetime(df['date']).dt.date
+
+        return df
+    
+
+    def saveDf2Csv(self, df, folderPath, fileName):
+        '''
+        Save a DataFrame to a CSV file.
+
+        Parameters:
+            df (pd.DataFrame): The DataFrame to be saved.
+            file_path (str): The path to the CSV file where the DataFrame will be saved.
+        Returns:
+            bool: True if the DataFrame was successfully saved, False otherwise.
+        '''
+        fullCsvFilePath = os.path.join(folderPath, fileName)
+
+        try:
+            # Set index=False to exclude the index column
+            df.to_csv(fullCsvFilePath, index=False)  
+
+            print(f"DataFrame saved to {fullCsvFilePath}")
+
+            return True
+        
+        except Exception as e:
+            print(f"Error saving DataFrame to {fullCsvFilePath}: {str(e)}")
+            return False
